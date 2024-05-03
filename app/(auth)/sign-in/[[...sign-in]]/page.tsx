ea7c9c233f8d639d7d51cbfc6257fcd41ac6068c
kumar <kumarsatyam7631901399@gmail.com>
2024-05-03 06:33:59 +0530

    initial commit

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,15 @@
+import { SignIn } from '@clerk/nextjs'
+
+const SignInPage = () => {
+  return <SignIn />
+  
+}
+
+export default SignInPage
+
+/*import { SignIn } from "@clerk/nextjs";
+
+export default function Page() {
+  return <SignIn path="/sign-in" />;
+}
+*/
---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,14 @@
+import { SignUp } from '@clerk/nextjs'
+
+const SignUpPage = () => {
+  return <SignUp />
+    
+}
+
+export default SignUpPage
+
+/*import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return <SignUp path="/sign-up" />;
+}*/
